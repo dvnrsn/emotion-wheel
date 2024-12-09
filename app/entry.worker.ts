93322232d4955503f08a6fcabbfc4e6769f3bfdd
= <=>
2024-12-09 11:30:58 -0700

    Add offline mode

--- app/entry.worker.ts
+++ app/entry.worker.ts
@@ -1,32 +1,81 @@
 /// <reference lib="WebWorker" />
 
-import type { WorkerDataFunctionArgs } from '@remix-pwa/sw';
+import type { DefaultFetchHandler } from "@remix-pwa/sw";
 
 export {};
 
 declare let self: ServiceWorkerGlobalScope;
 
-self.addEventListener('install', (event) => {
-  console.log('Service worker installed');
+import {
+  EnhancedCache,
+  isDocumentRequest,
+  isLoaderRequest,
+  Logger,
+} from "@remix-pwa/sw";
+
+const logger = new Logger({
+  prefix: "[Emotion Wheel]",
+});
+
+self.addEventListener("install", (event) => {
+  logger.log("Service worker installed");
 
   event.waitUntil(self.skipWaiting());
 });
 
-self.addEventListener('activate', (event) => {
-  console.log('Service worker activated');
+self.addEventListener("activate", (event) => {
+  logger.log("Service worker activated");
 
   event.waitUntil(self.clients.claim());
 });
 
-export const defaultFetchHandler = async ({
-  request,
-}: WorkerDataFunctionArgs) => {
-  console.log('test');
-  const serverHost = 'https://api.example.com';
+export const defaultFetchHandler: DefaultFetchHandler = async ({ context }) => {
+  const request = context.event.request;
   const url = new URL(request.url);
 
-  if (request.method.toLowerCase() === 'get' && url.searchParams.get('_data'))
-    return fetch(`${serverHost}${url.pathname}`);
+  if (isDocumentRequest(request)) {
+    return documentCache.handleRequest(request);
+  }
+
+  if (isLoaderRequest(request)) {
+    return dataCache.handleRequest(request);
+  }
+
+  if (self.__workerManifest.assets.includes(url.pathname)) {
+    return assetCache.handleRequest(request);
+  }
 
   return fetch(request);
 };
+
+const version = "v1";
+
+const DOCUMENT_CACHE_NAME = `document-cache`;
+const ASSET_CACHE_NAME = `asset-cache`;
+const DATA_CACHE_NAME = `data-cache`;
+
+const documentCache = new EnhancedCache(DOCUMENT_CACHE_NAME, {
+  version,
+  strategy: "CacheFirst",
+  strategyOptions: {
+    maxEntries: 64,
+  },
+});
+
+const assetCache = new EnhancedCache(ASSET_CACHE_NAME, {
+  version,
+  strategy: "CacheFirst",
+  strategyOptions: {
+    maxAgeSeconds: 60 * 60 * 24 * 90, // 90 days
+    maxEntries: 100,
+  },
+});
+
+const dataCache = new EnhancedCache(DATA_CACHE_NAME, {
+  version,
+  strategy: "NetworkFirst",
+  strategyOptions: {
+    networkTimeoutInSeconds: 10,
+    maxEntries: 72,
+  },
+});
